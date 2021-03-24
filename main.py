52573ec6f155c9be8ce3cafc4ad783d21de5ddad
Casey Y <another1thedust@gmail.com>
2021-03-24 21:36:41 +0000

    front end

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
-from flask import Flask, jsonify
-
+from flask import Flask, jsonify, render_template, request
 server = Flask(__name__)
+app = Flask('a')
 
-data = {
-  "message": "hello, world!"
-}
+data = "The message is " + "'hello world!'"
 
-@server.route('/') # URL
+def sendit():
+  if request.method == "SEND":
+    if request.form.get('send'):
+      return render_template('personal_user.html')
+    else:
+      return render_template('page.html')
+  elif request.method == "GET":
+    #dosomething
+    pass
+@app.route('/')
 def index():
-	return jsonify(data)
+	return render_template('page.html')
+  
 
-@server.route('/2/') # URL
-def two():
-	return "Route 2!"
+@app.route('/userpage')
+def userpage():
+  return render_template('personal_user.html')
 
-server.run(host='0.0.0.0', port=8080)
+
+app.run('0.0.0.0',8080)
